b432c9b71db9c822e165d385425b3baa14947982
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2024-01-08 16:09:33 +0900

    chore : conflict 해결

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -4,6 +4,8 @@ import MemberPage from "../../pages/MemberPage";
 import BusPage from "../../pages/Bus/BusPage";
 import BusListPage from "../../pages/Bus/BusListPage";
 import BusDatePage from "../../pages/Bus/BusDatePage";
+import ScheduleMange from "../Schedule/ScheduleManage";
+import ScheduleTimeTable from "../Schedule/ScheduleTimeTable";
 
 const Router = () => {
   return (
@@ -13,6 +15,8 @@ const Router = () => {
       <Route path="/bus" element={<BusPage />} />
       <Route path="/bus-list" element={<BusListPage />} />
       <Route path="/bus-date" element={<BusDatePage />} />
+      <Route path="/schedule" element={<ScheduleMange />} />
+      <Route path="/schedule-time-table" element={<ScheduleTimeTable />} />
     </Routes>
   );
 };
