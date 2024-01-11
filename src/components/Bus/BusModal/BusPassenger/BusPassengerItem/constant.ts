808a11f15a68d5258a463ded83e9cfcff702e60f
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-01-11 12:39:04 +0900

    chore : constant 파일을 컴포넌트 폴더로 이동

---
+++ src/components/Bus/BusModal/BusPassenger/BusPassengerItem/constant.ts
@@ -0,0 +1,6 @@
+export const BUS_PASSENGER_TITLE_ITEMS = [
+  "이름",
+  "아이디",
+  "전화번호",
+  "탑승버스",
+];
---
+++ src/components/Bus/BusModal/BusRegister/BusRegisterForm/constant.ts
@@ -0,0 +1,7 @@
+export const BUS_REGIST_TITLE_ITEMS = [
+  "버스 이름",
+  "버스 설명",
+  "출발시간",
+  "소요시간",
+  "인원제한",
+];
