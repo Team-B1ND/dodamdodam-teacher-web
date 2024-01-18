9f663431d78596f69156ab50cb6ae7c397749944
전해림 <HAERIM060920@gmail.com>
2024-01-18 15:31:06 +0900

    refactor : provider 전역 컴포넌트로 수정 #4

--- src/pages/Bus/BusDatePage.tsx
+++ src/pages/Bus/BusDatePage.tsx
@@ -1,14 +1,14 @@
 import BusDate from "../../components/Bus/BusDate";
-import BusProvider from "../../components/Bus/BusProvider";
+import SectionHeaderProvider from "../../components/common/SectionHeaderProvider";
 
 const BusDatePage = () => {
   return (
-    <BusProvider
+    <SectionHeaderProvider
       title="날짜별 버스 조회"
       subTitle="원하는 날짜를 선택하여 버스를 조회할 수 있습니다."
     >
       <BusDate />
-    </BusProvider>
+    </SectionHeaderProvider>
   );
 };
 
--- src/pages/Bus/BusPage.tsx
+++ src/pages/Bus/BusPage.tsx
@@ -1,14 +1,14 @@
 import BusManagement from "../../components/Bus/BusManagement";
-import BusProvider from "../../components/Bus/BusProvider";
+import SectionHeaderProvider from "../../components/common/SectionHeaderProvider";
 
 const BusPage = () => {
   return (
-    <BusProvider
+    <SectionHeaderProvider
       title="버스 관리"
       subTitle="버스를 추가 / 수정 / 삭제하여 관리할 수 있습니다."
     >
       <BusManagement />
-    </BusProvider>
+    </SectionHeaderProvider>
   );
 };
 
