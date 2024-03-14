e9d6c50345220c3601210aa3f4be2beec52d19a1
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-03-14 10:02:39 +0900

    refactor: SigninReponse 수정

--- src/types/Signin/signin.type.ts
+++ src/types/Signin/signin.type.ts
@@ -1,7 +1,9 @@
 import { Member } from "types/Member/member.type";
 
 export interface SigninResponse extends Response {
-  member: Member;
-  refreshToken: string;
-  accessToken: string;
+  data: {
+    member: Member;
+    refreshToken: string;
+    accessToken: string;
+  };
 }
