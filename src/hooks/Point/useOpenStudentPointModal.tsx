31a467e5365ba2032a1675c5b6235f51dd2e090f
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2024-03-28 09:05:01 +0900

    chore : 상벌점 모달 props 추가

--- src/hooks/Point/useOpenStudentPointModal.tsx
+++ src/hooks/Point/useOpenStudentPointModal.tsx
@@ -1,23 +1,30 @@
 import { useOverlay } from "@toss/use-overlay";
 import StudentPointInfoModal from "components/Point/Modal/StudentPointInfoModal";
 import Modal from "components/common/Modal";
+import { PointType } from "types/Point/types";
 
 export const useOpenStudentPointInfoModal = () => {
   const StudnetPointInfoOverlay = useOverlay();
 
   const openStudentPointInfoModalOverlay = (
     modal:
-      | { type: "studentInfo"; studentId: number }
+      | { type: "studentInfo"; studentId: number; pointType: PointType }
       | { type: "modify"; pointReasonId: number }
   ) => {
     return new Promise((resolve) => {
       StudnetPointInfoOverlay.open(({ isOpen, close }) =>
         modal.type === "modify" ? (
           <Modal title="학생 상벌점 정보" isOpen={isOpen} close={close}>
-            <StudentPointInfoModal close={close} />
+            <></>
           </Modal>
         ) : (
-          <div>학생 상벌점 정보 모달</div>
+          <Modal title="학생 상벌점 정보" isOpen={isOpen} close={close}>
+            <StudentPointInfoModal
+              pointType={modal.pointType}
+              studentId={modal.studentId}
+              close={close}
+            />
+          </Modal>
         )
       );
     });
