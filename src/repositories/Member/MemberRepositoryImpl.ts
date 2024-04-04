903ed869c6ff2e219e7d1a276aa80dfd6c149284
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-04-04 22:05:59 +0900

    refactor: 선생님 회원가입 axios 수정

--- src/repositories/Member/MemberRepositoryImpl.ts
+++ src/repositories/Member/MemberRepositoryImpl.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { MemberRepository, MemberSignUpParam } from "./MemberRepository";
 import { dodamTestAxios, dodamV6Axios } from "libs/Axios/customAxios";
 import { MemberResponse, MyMemberResponse } from "types/Member/member.type";
@@ -9,7 +10,10 @@ class MemberRepositoryImpl implements MemberRepository {
   }
 
   public async postMemberJoinTeacher(param: MemberSignUpParam): Promise<void> {
-    await dodamTestAxios.post("/member/join-teacher", param);
+    await axios.post(
+      `${process.env.REACT_APP_DODAM_TEST_SERVER_V6}/member/join-teacher`,
+      param
+    );
   }
 
   public async getMyMember(): Promise<MyMemberResponse> {
