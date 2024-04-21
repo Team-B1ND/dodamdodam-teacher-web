bd4032e2e548656138d7bdf17c2f732b46436afc
박상현 <sanghyun9467@gmail.com>
2024-04-21 14:56:18 +0900

    feat: add bus error handling logic (#53)

--- src/hooks/Bus/useDeleteBus.ts
+++ src/hooks/Bus/useDeleteBus.ts
@@ -3,6 +3,7 @@ import { QUERY_KEYS } from "queries/queryKey";
 import { useQueryClient } from "react-query";
 import { BusDateParam } from "repositories/Bus/BusRepository";
 import { useDeleteBusMutation } from "../../queries/Bus/bus.query";
+import { AxiosError } from "axios";
 
 export const useDeleteBus = () => {
   const deleteBus = useDeleteBusMutation();
@@ -21,8 +22,9 @@ export const useDeleteBus = () => {
           queryClient.invalidateQueries(QUERY_KEYS.bus.busDate(busDate));
           B1ndToast.showSuccess("버스를 삭제하였습니다.");
         },
-        onError: () => {
-          B1ndToast.showError("버스를 삭제하지 못했습니다.");
+        onError: (error) => {
+          const errorResponse = (error as AxiosError).response;
+          B1ndToast.showError((errorResponse?.data as AxiosError).message);
         },
       });
     }
