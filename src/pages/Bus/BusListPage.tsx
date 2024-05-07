2b8049f0d522be3836f653a618b27d42c0921cbe
전해림 <HAERIM060920@gmail.com>
2024-05-07 11:18:08 +0900

    refactor : 버스 title 네이밍 수정

--- src/pages/Bus/BusListPage.tsx
+++ src/pages/Bus/BusListPage.tsx
@@ -5,7 +5,7 @@ const BusListPage = () => {
   return (
     <SectionHeaderProvider
       title="버스 전체 기록 조회"
-      subTitle="원하는 날짜를 선택하여 버스를 조회할 수 있습니다."
+      subTitle="버스 전체 기록을 조회할 수 있습니다."
     >
       <BusList />
     </SectionHeaderProvider>
