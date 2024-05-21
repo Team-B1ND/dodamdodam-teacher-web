0f27576bdcf1af3e1b86b723b572a8a6e0037864
jeonminchan <132974029+wjzlskxk@users.noreply.github.com>
2024-05-21 21:29:50 +0900

    design :: 학사일정 TableView 일정 간격 조정 #58

--- src/components/Schedule/ScheduleManage/TableView/index.tsx
+++ src/components/Schedule/ScheduleManage/TableView/index.tsx
@@ -5,7 +5,7 @@ import { useGetSchedulesQuery } from "queries/Schedule/schedule.query";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
 import dataTransform from "utils/Transform/dataTransform";
-import { TRStyle } from "./style";
+import { TBodyStyle, TRStyle } from "./style";
 import useDeleteSchedule from "hooks/Schedule/useDeleteSchedule";
 
 const TableView = () => {
@@ -20,20 +20,15 @@ const TableView = () => {
   }, [inView, fetchNextPage]);
 
   return (
-    <TableAttribute
-      constant={SCHEDULE_TABLE_ITEMS}
-      thStyle={{ width: "10.5%" }}
-    >
-      <TBody>
+    <TableAttribute constant={SCHEDULE_TABLE_ITEMS} thStyle={{ width: "10.5%" }}>
+      <TBody customStyle={TBodyStyle}>
         {scheduleData?.pages.map((page) =>
           page.data.map((schedule) => (
             <TR customStyle={TRStyle}>
-              <TD customStyle={{ width: "11.5%" }}>
-                <Button ButtonType="agree">
-                  {schedule.type === "ACADEMIC" ? "학사 일정" : "휴일"}
-                </Button>
+              <TD customStyle={{ width: "10.5%" }}>
+                <Button ButtonType="agree">{schedule.type === "ACADEMIC" ? "학사 일정" : "휴일"}</Button>
               </TD>
-              <TD customStyle={{ width: "11%" }}>{schedule.name}</TD>
+              <TD customStyle={{ width: "10.5%" }}>{schedule.name}</TD>
               <TD customStyle={{ width: "10.5%" }}>
                 {schedule.targetGrades.map((grade, idx) => (
                   <>
@@ -45,16 +40,13 @@ const TableView = () => {
               <TD customStyle={{ width: "10.5%" }}>{schedule.place}</TD>
               <TD customStyle={{ width: "10.5%" }}>{schedule.date[0]} </TD>
               <TD customStyle={{ width: "10.5%" }}>{schedule.date[1]} </TD>
-              <TD customStyle={{ width: "8.5%" }}>
-                <Button
-                  ButtonType="disagreed"
-                  onClick={() => onDeleteSchedule(schedule.id)}
-                >
+              <TD customStyle={{ width: "10.5%" }}>
+                <Button ButtonType="disagreed" onClick={() => onDeleteSchedule(schedule.id)}>
                   삭제
                 </Button>
               </TD>
             </TR>
-          ))
+          )),
         )}
       </TBody>
       <div ref={ref}></div>
--- src/components/Schedule/ScheduleManage/TableView/style.ts
+++ src/components/Schedule/ScheduleManage/TableView/style.ts
@@ -7,4 +7,11 @@ export const TRStyle: CSSObject = {
   borderBottom: "1px solid #d9d9d9",
   fontSize: "14px",
   fontWeight: "600",
+  display: "flex",
+  justifyContent: "space-between",
+};
+
+export const TBodyStyle: CSSObject = {
+  display: "flex",
+  flexDirection: "column",
 };
