5d8be0c93a948e99102efab3a4253f2dffddeb36
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-09-10 18:03:19 +0900

    refactor :: change load member api

--- src/repositories/Member/MemberRepositoryImpl.ts
+++ src/repositories/Member/MemberRepositoryImpl.ts
@@ -2,19 +2,16 @@ import axios from "axios";
 import { MemberRepository, MemberSignUpParam } from "./MemberRepository";
 import { dodamAxios } from "libs/Axios/customAxios";
 import { MemberResponse, MyMemberResponse } from "types/Member/member.type";
-import CONFIG from "../../config/config.json"
+import CONFIG from "../../config/config.json";
 
 class MemberRepositoryImpl implements MemberRepository {
   public async getAllMemberList(): Promise<MemberResponse> {
-    const { data } = await dodamAxios.get("/member/all");
+    const { data } = await dodamAxios.get("/member/status?status=ACTIVE");
     return data;
   }
 
   public async postMemberJoinTeacher(param: MemberSignUpParam): Promise<void> {
-    await axios.post(
-      `${CONFIG.SERVER}/member/join-teacher`,
-      param
-    );
+    await axios.post(`${CONFIG.SERVER}/member/join-teacher`, param);
   }
 
   public async getMyMember(): Promise<MyMemberResponse> {
