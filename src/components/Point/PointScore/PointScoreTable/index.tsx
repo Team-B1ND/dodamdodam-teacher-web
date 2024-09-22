cc6ad4beff27e14efb522153cf602157201d2cd8
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-09-22 19:34:15 +0900

    feat :: Point to CSVData #72

--- src/components/Point/PointScore/PointScoreTable/index.tsx
+++ src/components/Point/PointScore/PointScoreTable/index.tsx
@@ -2,70 +2,35 @@ import profileImg from "assets/profileImg.svg";
 import TableAttribute from "components/common/TableAttribute";
 import { POINT_SCORE_ITEMS } from "../constant";
 import { Button, TBody, TD } from "@b1nd/b1nd-dodamdodam-ui";
-import {
-  MemberImage,
-  MemberItemTR,
-  MemberTD,
-} from "components/Member/MemberItem/style";
-import { useGetPointAllMemberQuery } from "queries/Point/point.query";
+import { MemberImage, MemberItemTR, MemberTD } from "components/Member/MemberItem/style";
 import styled, { CSSObject } from "styled-components";
 import { useOpenStudentPointInfoModal } from "hooks/Point/useOpenStudentPointModal";
 import { PointType } from "types/Point/point.type";
-import { sortStudentGrade } from "utils/Member/sortStudentGrade";
-import { searchName } from "utils/common/searchName";
-import { useRecoilValue } from "recoil";
-import {
-  PointSelectGrade,
-  PointSelectRoom,
-  PointStduentSearch,
-} from "stores/Point/point.store";
-import { changeGrade, changeRoom } from "utils/Member/changeGrade";
 import useSetPointStudentIds from "hooks/Point/useSetPointStudentIds";
+import { PointDataToCsvData } from "utils/Transform/csvTransform";
+import { Suspense } from "react";
 
 interface Props {
   pointQueryParam: string;
 }
 
 const PointScoreTable = ({ pointQueryParam }: Props) => {
-  const searchValue = useRecoilValue(PointStduentSearch);
-  const selectGrade = useRecoilValue(PointSelectGrade);
-  const selectRoom = useRecoilValue(PointSelectRoom);
-
-  const { data: studentPointScoreData } = useGetPointAllMemberQuery(
-    pointQueryParam!,
-    { suspense: true }
-  );
-
   const { onSetStudentList, studentIds } = useSetPointStudentIds();
-
+  const { pointData } = PointDataToCsvData(pointQueryParam);
   const { openStudentPointInfoModalOverlay } = useOpenStudentPointInfoModal();
 
   return (
-    <TableAttribute constant={POINT_SCORE_ITEMS}>
-      <TBody customStyle={TBodyStyle}>
-        {studentPointScoreData?.data
-          .sort((student1, student2) =>
-            sortStudentGrade(student1.student!, student2.student)
-          )
-          .filter(
-            (data) =>
-              data.student.grade === changeGrade(selectGrade) ||
-              changeGrade(selectGrade) === 0
-          )
-          .filter(
-            (data) =>
-              data.student?.room === changeRoom(selectRoom) ||
-              changeRoom(selectRoom) === 0
-          )
-          .filter((data) => searchName(data.student.name, searchValue))
-          .map((data) => (
+    <Suspense fallback={<>...loading</>}>
+      <TableAttribute constant={POINT_SCORE_ITEMS}>
+        <TBody customStyle={TBodyStyle}>
+          {pointData?.map((data) => (
             <TR
               customStyle={MemberItemTR}
               isClicked={studentIds.includes(data.student.id) ? true : false}
               onClick={() => {
                 onSetStudentList(
                   data.student.id,
-                  `${data.student.grade}학년 ${data.student.room}반 ${data.student.number}번 ${data.student.name}`
+                  `${data.student.grade}학년 ${data.student.room}반 ${data.student.number}번 ${data.student.name}`,
                 );
               }}
             >
@@ -74,15 +39,12 @@ const PointScoreTable = ({ pointQueryParam }: Props) => {
               </TD>
               <TD customStyle={MemberTD}>{data.student.name}</TD>
               <TD customStyle={MemberTD}>
-                {data.student.grade}학년 {data.student.room}반
-                {data.student.number}번
+                {data.student.grade}학년 {data.student.room}반{data.student.number}번
               </TD>
               <TD customStyle={MemberTD}>{data.bonus}점</TD>
               <TD customStyle={MemberTD}>{data.minus}점</TD>
               <TD customStyle={MemberTD}>{data.offset}점 </TD>
-              <TD customStyle={MemberTD}>
-                {data.bonus - data.minus + data.offset}점
-              </TD>
+              <TD customStyle={MemberTD}>{data.bonus - data.minus + data.offset}점</TD>
               <TD>
                 <Button
                   ButtonType="disagree"
@@ -100,8 +62,9 @@ const PointScoreTable = ({ pointQueryParam }: Props) => {
               </TD>
             </TR>
           ))}
-      </TBody>
-    </TableAttribute>
+        </TBody>
+      </TableAttribute>
+    </Suspense>
   );
 };
 
@@ -111,8 +74,7 @@ const TR = styled.tr<{ customStyle?: CSSObject; isClicked: boolean }>`
   border-top: 1px solid black;
   border-bottom: 1px solid black;
   cursor: pointer;
-  background-color: ${({ isClicked }) =>
-    isClicked ? "rgba(0, 103, 188, .08)" : "#fff"};
+  background-color: ${({ isClicked }) => (isClicked ? "rgba(0, 103, 188, .08)" : "#fff")};
 
   ${({ customStyle }) => customStyle}
 `;
--- src/components/Point/PointScore/index.tsx
+++ src/components/Point/PointScore/index.tsx
@@ -4,24 +4,30 @@ import PointProvider from "../PointProvider";
 import { useSearchParams } from "react-router-dom";
 import ErrorBoundary from "components/common/ErrorBoundary";
 import { Suspense } from "react";
-import SkeletonComponent from "components/common/Skeleton";
 
 const PointScore = () => {
   const [searchParam] = useSearchParams();
   const pointQueryParam = searchParam.get("type");
 
   return (
-    <PointProvider
-      title={pointQueryParam === "DORMITORY" ? " 기숙사 상벌점" : "학교 상벌점"}
-      subTitle="학생 상벌점 조회, 발급, 삭제가 가능합니다."
-    >
-      <PointScoreHeader pointQueryParam={pointQueryParam} />
-      <ErrorBoundary fallback={<>error</>}>
-        <Suspense fallback={<SkeletonComponent height={60} />}>
-          <PointScoreTable pointQueryParam={pointQueryParam!} />
-        </Suspense>
-      </ErrorBoundary>
-    </PointProvider>
+    <ErrorBoundary fallback={<>...loading</>}>
+      <Suspense fallback={<>...loading</>}>
+        <PointProvider
+          title={pointQueryParam === "DORMITORY" ? " 기숙사 상벌점" : "학교 상벌점"}
+          subTitle="학생 상벌점 조회, 발급, 삭제가 가능합니다."
+        >
+          <ErrorBoundary fallback={<>...loading</>}>
+            <Suspense fallback={<>...loading</>}></Suspense>
+            <PointScoreHeader pointQueryParam={pointQueryParam!} />
+          </ErrorBoundary>
+          <ErrorBoundary fallback={<>...loading</>}>
+            <Suspense fallback={<>...loading</>}>
+              <PointScoreTable pointQueryParam={pointQueryParam!} />
+            </Suspense>
+          </ErrorBoundary>
+        </PointProvider>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
