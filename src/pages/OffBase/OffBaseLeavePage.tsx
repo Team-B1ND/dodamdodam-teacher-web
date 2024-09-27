4a29fdc1758bbd3116cbbeb2e8e2ac66af910f0c
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-09-27 16:00:28 +0900

    fix :: OffBasePass Header description #79

--- src/pages/OffBase/OffBaseLeavePage.tsx
+++ src/pages/OffBase/OffBaseLeavePage.tsx
@@ -3,7 +3,7 @@ import SectionHeaderProvider from "components/common/SectionHeaderProvider";
 
 const OffBaseLeavePage = () => {
   return (
-    <SectionHeaderProvider title="외박" subTitle="외박한 학생이 보여집니다.">
+    <SectionHeaderProvider title="외박" subTitle="현재시간을 기준으로 외박승인을 받지못했거나 또는 외박중인 학생이 보여집니다.">
       <OffBaseLeave />
     </SectionHeaderProvider>
   );
