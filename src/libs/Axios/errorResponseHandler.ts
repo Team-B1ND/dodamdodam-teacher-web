760c8b9a0de35ce62904d53e48001a7193b4bbee
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-15 19:42:49 +0900

    chore :: pre-commit

--- src/libs/Axios/errorResponseHandler.ts
+++ src/libs/Axios/errorResponseHandler.ts
@@ -1,12 +1,8 @@
-import { AxiosError } from "axios";
-import Token from "../Token/Token";
-import {
-  ACCESS_TOKEN_KEY,
-  REFRESH_TOKEN_KEY,
-  REQUEST_TOKEN_KEY,
-} from "constants/Token/Token.constant";
-import { dodamAxios } from "./customAxios";
-import AuthRepositoryImpl from "repositories/Auth/AuthRepositoryImpl";
+import { AxiosError } from 'axios';
+import Token from '../Token/Token';
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY, REQUEST_TOKEN_KEY } from 'constants/Token/Token.constant';
+import { dodamAxios } from './customAxios';
+import AuthRepositoryImpl from 'repositories/Auth/AuthRepositoryImpl';
 
 export const errorResponseHandler = async (error: AxiosError) => {
   if (error.response) {
@@ -17,21 +13,16 @@ export const errorResponseHandler = async (error: AxiosError) => {
     const usingAccessToken = Token.getToken(ACCESS_TOKEN_KEY);
     const usingRefreshToken = Token.getToken(REFRESH_TOKEN_KEY);
 
-    if (
-      usingAccessToken !== undefined &&
-      usingRefreshToken !== undefined &&
-      status === 401
-    ) {
+    if (usingAccessToken !== undefined && usingRefreshToken !== undefined && status === 401) {
       try {
-        const { data: newAccessToken } =
-          await AuthRepositoryImpl.refreshAccessToken(usingRefreshToken);
+        const { data: newAccessToken } = await AuthRepositoryImpl.refreshAccessToken(usingRefreshToken);
 
         Token.setToken(ACCESS_TOKEN_KEY, newAccessToken);
 
-        dodamAxios.defaults.headers.common[
-          REQUEST_TOKEN_KEY
-        ] = `Bearer ${newAccessToken}`;
-      } catch (error) {}
+        dodamAxios.defaults.headers.common[REQUEST_TOKEN_KEY] = `Bearer ${newAccessToken}`;
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
   return Promise.reject(error);
