0d659742359e6461aef226a16a5b8c4963d0e240
ftery0 <lerb5253@dgsw.hs.kr>
2025-02-11 17:21:31 +0900

    feat :: searchbar #95

---
+++ src/hooks/Notice/useNotice.ts
@@ -0,0 +1,11 @@
+import { useRef } from "react";
+export const useNotice = () =>{
+    const searchRef = useRef<HTMLInputElement>(null);
+    const searchSubmit = () => {
+        console.log("검색어 post");   
+    }
+    return{
+        searchRef,
+        searchSubmit,
+    }
+}
--- src/pages/Notice/Main/noticePages.tsx
+++ src/pages/Notice/Main/noticePages.tsx
@@ -1,11 +1,18 @@
 import * as S from "./style";
 import NoticeSearchBar from "components/common/NoticeSearchbar";
+import { useNotice } from "hooks/Notice/useNotice";
 
 const NoticePage = () => {
+    const {...notice} = useNotice();
     return(
     
         <S.NoticeBox>
-            <NoticeSearchBar/>
+            <NoticeSearchBar  
+                ref={notice.searchRef} 
+                onKeyDown={notice.searchSubmit}  
+                searchFn={notice.searchSubmit}
+            />
+            
         </S.NoticeBox>
     
     )
