73b28157d822f9f1140c3a034ceefd498a09b7f9
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2025-02-13 17:50:03 +0900

    feat :: NoticeSidebar View #96

--- src/components/Notice/NoticeSidebar/index.tsx
+++ src/components/Notice/NoticeSidebar/index.tsx
@@ -1,8 +1,32 @@
-const NoticeSidebar = () => {
-    return (
-        <>
-    
-        </>
-    )
-}
+import React from "react";
+import * as S from "./style";
+import { NOTICE_SIDEBAR_ITEMS } from "constants/Notice/notice.constant";
+import { NoticeSidebarType } from "types/Notice/notice.type";
+import { DodamSegmentedButton } from "@b1nd/dds-web";
+
+const NoticeSidebar = ({ title }: NoticeSidebarType) => {
+  return (
+    <S.NoticeSidebarWrap>
+      <DodamSegmentedButton
+        num={NOTICE_SIDEBAR_ITEMS.length}
+        type="block"
+        data={NOTICE_SIDEBAR_ITEMS}
+        width={404}
+        height={50}
+        textColor="staticWhite"
+        customBackbgroundColor="primaryNormal"
+        customBackbgroundWrapColor="staticWhite"
+      />
+      <S.CategoryWrap>
+        <S.Title>{title}</S.Title>
+        <S.Category>
+          {Array.from({ length: 10 }).map(() => (
+            <S.CategoryTag>카테고리</S.CategoryTag>
+          ))}
+        </S.Category>
+        <S.Button>완료</S.Button>
+      </S.CategoryWrap>
+    </S.NoticeSidebarWrap>
+  );
+};
 export default NoticeSidebar;
--- src/components/common/NoticePageTemplate/index.tsx
+++ src/components/common/NoticePageTemplate/index.tsx
@@ -8,7 +8,7 @@ const NoticePageTemplate = () => {
   return (
     <NoticContainer>
         <Outlet /> 
-        <NoticeSidebar />
+        <NoticeSidebar title="카테고리를 선택해보세요!" />
     </NoticContainer>
   );
 };
@@ -19,6 +19,7 @@ export default NoticePageTemplate;
 export const NoticContainer = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
   padding: 1rem 2.5rem;
   white-space: nowrap;
   background-color: ${({theme})=> theme.backgroundAlternative};
