42c01a6589edf12d38ef772f6fbe39a473f93672
ftery0 <lerb5253@dgsw.hs.kr>
2025-02-13 16:35:16 +0900

    refactor :: skeleton components

--- src/components/common/Skeleton/style.ts
+++ src/components/common/Skeleton/style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled,{CSSObject} from "styled-components";
 import { skeletonAnimtaion } from "@b1nd/b1nd-styled-components-util";
 
 export const BusSkeletonContainer = styled.div`
@@ -8,8 +8,12 @@ export const BusSkeletonContainer = styled.div`
   margin-top: 14px;
 `;
 
-export const BusSkeletonItem = styled.div<{ height: number }>`
+export const BusSkeletonItem = styled.div<{ 
+  height: number, 
+  customStyle: CSSObject 
+  }>`
   width: 100%;
   height: ${(props) => props.height}px;
   ${skeletonAnimtaion};
+  ${({ customStyle }) => customStyle}
 `;
