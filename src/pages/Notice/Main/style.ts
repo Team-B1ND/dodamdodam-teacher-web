aceb5505c01dae472bda78ccf79a83a0f24331cf
ftery0 <lerb5253@dgsw.hs.kr>
2025-02-15 17:22:54 +0900

    refactor :: fit style size

--- src/pages/Notice/Main/style.ts
+++ src/pages/Notice/Main/style.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const NoticeContainer = styled.div`
     display: flex;
     flex-direction: column;
-    width: 60%;
+    width: 65%;
     height: 100%;
     gap: 12px;
     padding: 12px 16px 0 16px;
