059aea957085528250a209382969f87c54163939
dlskl <skyandme77@gmail.com>
2025-05-14 19:47:33 +0900

    style : add night study ban font #144

--- src/components/NightStudy/NightStudyBan/NightStudyBanItem/Student/style.ts
+++ src/components/NightStudy/NightStudyBan/NightStudyBanItem/Student/style.ts
@@ -1,5 +1,7 @@
-import {CSSObject} from "styled-components";
+import { CSSObject } from "styled-components";
 
 export const StudentTD: CSSObject = {
   width: "11.5%",
-}
+  fontSize: "16px",
+  fontWeight: "600",
+};
